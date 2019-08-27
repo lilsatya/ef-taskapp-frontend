5835e4e684c3db57a2a19be4db77097e6fd12b89
Lingga <lingga@xcidic.com>
2019-08-27 14:33:07 +0700

    Updates

---
+++ precache-manifest.a28cf67a87f6bb7dc50c98b67d61ce92.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e027e2e1a4f48dc92ef5da9acb4a01db",
+    "url": "/ef-taskapp-frontend/index.html"
+  },
+  {
+    "revision": "5e0917717bbadfa66c5f",
+    "url": "/ef-taskapp-frontend/static/css/2.4482b0e7.chunk.css"
+  },
+  {
+    "revision": "372e3ddf11bd8287c10b",
+    "url": "/ef-taskapp-frontend/static/css/4.2d4bc2cf.chunk.css"
+  },
+  {
+    "revision": "d2395f2e0ff2e0ea61b3",
+    "url": "/ef-taskapp-frontend/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5e0917717bbadfa66c5f",
+    "url": "/ef-taskapp-frontend/static/js/2.10e8c0bd.chunk.js"
+  },
+  {
+    "revision": "6bec71b28f9d0534c2c8",
+    "url": "/ef-taskapp-frontend/static/js/3.671a6c3c.chunk.js"
+  },
+  {
+    "revision": "372e3ddf11bd8287c10b",
+    "url": "/ef-taskapp-frontend/static/js/4.ff3a7455.chunk.js"
+  },
+  {
+    "revision": "952d88351d96a59b6a54",
+    "url": "/ef-taskapp-frontend/static/js/5.fab32d49.chunk.js"
+  },
+  {
+    "revision": "d2395f2e0ff2e0ea61b3",
+    "url": "/ef-taskapp-frontend/static/js/main.1ac212b6.chunk.js"
+  },
+  {
+    "revision": "b1dd7d81ce1cdbefccbe",
+    "url": "/ef-taskapp-frontend/static/js/runtime~main.20b4df6e.js"
+  }
+]);
